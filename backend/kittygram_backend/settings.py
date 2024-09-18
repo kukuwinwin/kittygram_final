a36c7e7fa6aa5a45da4e25ae57c68fd254aa8da9
kiwinwin <galia.baibulova98@gmail.com>
2024-09-18 09:39:05 +0300

    18_09

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', False).lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
